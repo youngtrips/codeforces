1ccb66baf90a67f4caae1f6b6b588c1cfc17ba73
dongchaoming <hi@cmdong.com>
2024-10-21 14:54:08 +0800

    recurse

--- 231a.ml
+++ 231a.ml
@@ -1,11 +1,10 @@
 let input () = Scanf.scanf "%d %d %d\n" (fun x y z -> (x, y, z))
 
 let rec solve n total =
-  match n with
-  | 0 -> total
-  | _ ->
-      let x, y, z = input () in
-      if x + y + z >= 2 then solve (n - 1) (total + 1) else solve (n - 1) total
+  if n <= 0 then total
+  else
+    let x, y, z = input () in
+    if x + y + z >= 2 then solve (n - 1) (total + 1) else solve (n - 1) total
 
 let main () =
   let n = read_int () in
