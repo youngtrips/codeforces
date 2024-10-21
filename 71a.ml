00e566584c608c9e74ec75102b65c42bda214011
dongchaoming <hi@cmdong.com>
2024-10-21 09:49:23 +0800

    initialize commit

---
+++ 71a.ml
@@ -0,0 +1,13 @@
+let solve s =
+  let size = String.length s in
+  if size <= 10 then Printf.printf "%s\n" s
+  else Printf.printf "%c%d%c\n" s.[0] (size - 2) s.[size - 1]
+
+let main () =
+  let n = int_of_string (read_line ()) in
+  for i = 1 to n do
+    let s = read_line () in
+    solve s
+  done
+
+let () = main ()
